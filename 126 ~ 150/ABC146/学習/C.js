6793fc3fee42045ed3968f0c535a4802a9b0e808
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-06 10:35:22 +0900

    ABC146C

--- 126 ~ 150/ABC146/学習/C.js
+++ 126 ~ 150/ABC146/学習/C.js
@@ -1,22 +1,55 @@
 // TODO
-//*   ABC 149 C - Next Prime
+//*   ABC 146 C - Buy an Integer
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let x = parseInt(input, 10);
-    for(let i = x; i <= 100003; i++) {
-        if(primeNumber(i)) return console.log(i);
+    const [a, b, x] = input.trim().split(' ').map(n => BigInt(n));
+
+    let left = 0n;
+    let right = BigInt(10e8 + 1);
+    
+    let count = 1;
+    
+    console.log('right', right);
+    console.log('---------------------');
+
+    //* 右と左の境界線がギリギリまで
+    while(right - left > 1n) {
+        //* 中間地点を調べる
+        let middle = (right+left) / 2n;
+        //* 中間地点の整数の値段と、xを比較する
+        //* 比較結果をもとに、右左のどちらかを半減する
+        if(Calculation(middle) <= x) {
+            left = middle;
+        } else {
+            right = middle;
+        }
+        
+        console.log('count', count);
+        console.log('middle', middle);
+        console.log('calculaton', Calculation(middle));
+        console.log('x', x);
+        
+        console.log('left', left);
+        console.log('right', right);
+
+        count++;
+        console.log('---------------------');
+    }
+    console.log(left.toString());
+
+    //* ---------------------------------
+    //* 関数
+    function keta(n) {
+        return BigInt(n.toString().split('').length); 
+    }
+    
+    function Calculation(c) {
+        return a * c + b * keta(c); 
     }
 }
 
-function primeNumber(n) {
-    if (n === 2) return true;
-    for (let i = 2; i < n; i++) {
-        if (n % i === 0) return false;
-    }
-    return true;
-}
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
--- 126 ~ 150/ABC146/提出/C_提出.js
+++ 126 ~ 150/ABC146/提出/C_提出.js
@@ -1,46 +1,39 @@
 // TODO
-//*   ABC 190 C - Bowls and Dishes
+//*   ABC 146 C - Buy an Integer
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const args = input.trim().split('\n');
-    const [n, M] = args[0].split(' ').map(n => parseInt(n, 10));
-    let ab = [];
-    for(let i = 1; i <= M; i++) {
-        ab.push(args[i].split(' ').map(n => parseInt(n, 10)));
+    const [a, b, x] = input.trim().split(' ').map(n => BigInt(n));
+
+    let left = 0n;
+    let right = BigInt(10e8 + 1);
+
+    //* 右と左の境界線がギリギリまで
+    while(right - left > 1n) {
+        //* 中間地点を調べる
+        let middle = (right+left) / 2n;
+        //* 中間地点の整数の値段と、xを比較する
+        //* 比較結果をもとに、右左のどちらかを半減する
+        if(Calculation(middle) <= x) {
+            left = middle;
+        } else {
+            right = middle;
+        }
     }
-    const k = args[M + 1]; 
-    let cd = [];
-    for(let i = M + 2; i < args.length; i++) {
-        cd.push(args[i].split(' ').map(n => parseInt(n, 10)));
+    console.log(left.toString());
+
+    //* ---------------------------------
+    //* 関数
+    function keta(n) {
+        return BigInt(n.toString().split('').length); 
     }
     
-    let answer = 0;
-    const k2 = 2 ** k;
-    
-    for(let i = 0; i < k2; i++) {
-        // 皿の作成
-        const dish = Array(n + 1).fill(0);
-
-        for(let j = 0; j < k; j++) {
-            if((i >> j) & 1) {
-                dish[cd[j][0]] += 1;
-            } else { 
-                dish[cd[j][1]] += 1;
-            }
-        }
-
-        let now = 0;
-        for(let k = 0; k < M; k++) {
-            if(dish[ab[k][0]] > 0 && dish[ab[k][1]] > 0) {
-                now += 1;
-            }
-        }
-        answer = Math.max(answer, now);
+    function Calculation(c) {
+        return a * c + b * keta(c); 
     }
-    console.log(answer);
 }
+
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
