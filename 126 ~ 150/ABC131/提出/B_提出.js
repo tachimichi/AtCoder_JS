38cdb71b7e1feedfebd23cf4a660fa0aacbacc09
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-13 14:25:04 +0900

    ABC

--- 126 ~ 150/ABC131/提出/B_提出.js
+++ 126 ~ 150/ABC131/提出/B_提出.js
@@ -1,23 +1,33 @@
 // TODO
-//*   ABC 132 B - Ordinary Number
+//*   ABC 131 B - Bite Eating
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict'
-    input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
-    const p = input[1].split(' ').map(n => parseInt(n, 10));
-    
-    let count = 0;
+    const [n, l] = input.trim().split(' ').map(n => parseInt(n, 10));
 
-    for(let i = 1; i < n-1; i++) {
-        if(p[i-1] < p[i] && p[i] < p[i+1]) count++;
-        if(p[i-1] > p[i] && p[i] > p[i+1]) count++;
+    let max = 0;
+    let count = 1;
+    let t = [];
+    for(let i = l; count <= n; i++) {
+        max += i;
+        count++;
+        t.push(i);
     }
-    console.log(count);
+
+    let min = 201;
+    let ans = 0;
+    for (let i = 0; i < t.length; i++) {
+        if(min > Math.abs(t[i])) {
+            min = Math.min(min, Math.abs(t[i]));
+            ans = t[i];
+        }
+    }
+
+    console.log(max - ans);
+
+
 }
-
-
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
