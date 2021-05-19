ab11b43029eb81f1a88c509cf152fbc6031ab0a1
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-20 08:45:21 +0900

    125

---
+++ 101 ~ 125/ABC125/提出/B_提出.js
@@ -0,0 +1,31 @@
+// TODO
+//*   ABC 125 B - Resale
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const v = input[1].split(' ').map(n => parseInt(n, 10));
+    const c = input[2].split(' ').map(n => parseInt(n, 10));
+    
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+        if(v[i] >c[i]) sum += v[i]-c[i];
+    }
+    console.log(sum);
+
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
---
+++ 101 ~ 125/ABC125/提出/C_提出.js
@@ -0,0 +1,37 @@
+// TODO
+//*   ABC 126 C - Dice and Coin
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const [n, k] = input.trim().split(' ').map(n => parseInt(n, 10));
+
+    let t = 0;
+    for(let i = 1;  i <= n; i++) {
+        let kakuritsu = 1/n;
+        let now = i;
+        while(now < k) {
+            now *= 2;
+            kakuritsu /= 2;
+            // console.log(now);
+        }
+        // console.log(kakuritsu);
+        t += kakuritsu;
+    }
+    console.log(t);
+
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
