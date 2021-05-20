fcd065e541de5711025fd86e9f193dd1bc6e872d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-20 09:33:34 +0900

    C問題 bit全探索

--- 176 ~ 200/ABC182/学習/C.js
+++ 176 ~ 200/ABC182/学習/C.js
@@ -4,41 +4,37 @@
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let n = parseInt(input, 10);
-    let f = input.split('').map(n => parseInt(n, 10));
-    //* ----------------------------
-    let single = 0;
-    let double = 0;
-    let d = 0;
-    let sum = 0;
-    let keta = 0;
+    // let a = parseInt(input, 10);
+    let a = input.trim();
+    let n = a.length;
 
-    while(n) {
-        d = n % 10;
-        if(d % 3 === 1) single++;
-        if(d % 3 === 2) double++;
-        n = Math.floor(n / 10);
-        sum += d;
-        keta++;
+    let min = 1e10;
+
+    //* bit全探索：1 ~ nの2進数まで
+    for (let i = 1; i < (1 << n); i++) { 
+        let t = [];
+        for (let j = 0; j < n; j++) {
+            //* メモ : 
+            //* (i & (1 << j)) = 0 というのは、i の j ビット目（2^j の位）が 0 であるための条件。
+            if (i & (1 << j)) {
+                t.push(a[j]);
+                // console.log(a[j]);
+                // console.log(t);
+            }
+        }
+        // console.log("t", t);
+        
+        let x = t.reduce((a, b) => parseInt(a,10)+parseInt(b,10),0);
+        if(x % 3 == 0) {
+            min = Math.min(min, n - t.length);
+        }
     }
 
-    // console.log(single, double);
-    // console.log(sum);
-    // console.log(keta);
-
-    if(sum % 3 === 0) return console.log(0);
-    if(n.length === 1) return console.log(-1);
-    
-    if (sum % 3=== 1) {
-        if(single >= 1 && keta > 1) return console.log(1);
-        if(double >= 2 && keta > 2) return console.log(2);
+    if (min == 1e10) {
+        console.log(-1);
+    } else {
+        console.log(min);
     }
-    else if(sum % 3 === 2) {
-        if(double >= 1 && keta > 1) return console.log(1);
-        if(single >= 2 && keta > 2) return console.log(2);
-    }
-    console.log(-1);
-
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
