9890de69c2256af51c479dd5393d4e349d8a4426
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-24 11:41:46 +0900

    C問題：愚直に走査

---
+++ 101 ~ 125/ABC121/学習/C.js
@@ -0,0 +1,33 @@
+// TODO
+//*   ABC 122 C - Energy Drink Collector
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const [n, m] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    let a = [];
+    for(let i = 1; i <= n; i++) {
+        const t = input[i].trim().split(' ').map(n => parseInt(n, 10));
+        a.push(t);
+    }
+    // console.log(a);
+    a.sort((a, b) => a[0] - b[0]);
+    // console.log(a);
+
+    let count = 0;
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < a[i][1]; j++) {
+            if(m > count) {
+                sum += a[i][0];
+                count++;
+            }
+        }
+    }
+    console.log(sum);
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
