cda056e6f02fe5b4316aee3ad0778f4e91a79a05
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-26 17:17:19 +0900

    gcd問題

--- 101 ~ 125/ABC118/学習/C.js
+++ 101 ~ 125/ABC118/学習/C.js
@@ -1,23 +1,28 @@
 // TODO
-//*   ABC 122 C - Energy Drink Collector
+//*   ABC 118 C - Monsters Battle Royale
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('').map(n => parseInt(n, 10));
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
 
-    let count_0 = 0;
-    let count_1 = 0;
-    for (let i = 0; i < input.length; i++) {
-        if (input[i] == 0) {
-            count_0++;
-        } else {
-            count_1++;
+    a.sort((a, b) => a - b);
+    let max = a[a.length - 1];
+    for (let i = 0; i < a.length - 1; i++) {
+        for (let j = i + 1; j < a.length; j++) {
+            max = Math.min(max, gcd(a[i], a[j]));
         }
     }
-    console.log(2 * Math.min(count_0, count_1));
+    console.log(max);
 }
 
+//* 2つの数字a, bの最大公約数を求める
+function gcd(a, b) {
+    if (b === 0) return a;
+    return gcd(b, a % b)
+}
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
--- 101 ~ 125/ABC118/提出/C_提出.js
+++ 101 ~ 125/ABC118/提出/C_提出.js
@@ -1,22 +1,32 @@
 // TODO
-//*   ABC 122 C - Energy Drink Collector
+//*   ABC 118 C - Monsters Battle Royale
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('').map(n => parseInt(n, 10));
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
 
-    let count_0 = 0;
-    let count_1 = 0;
-    for(let i = 0; i < input.length; i++) {
-        if(input[i] == 0) {
-            count_0++;
-        } else {
-            count_1++;
-        }
+    a.sort((a, b) => a - b);
+    //* 整数の足し引き⇒最大公約数を疑う
+    //* O(N + logA1) 
+    //* 
+    //* 
+    //* 
+    let max = a[0];
+    for (let i = 0; i < n; i++) {
+        max = gcd(max, a[i]);
     }
-    console.log(2* Math.min(count_0, count_1));
+    console.log(max);
 }
+
+//* 2つの数字a, bの最大公約数を求める
+function gcd(a, b) {
+    if (b === 0) return a;
+    return gcd(b, a % b)
+}
+
 
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
@@ -24,7 +34,7 @@ try {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
 } catch (error1) {
     try {
-    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+        main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
     } catch (error2) {
         console.log('error2', error2);
         console.log('----------------------------');
