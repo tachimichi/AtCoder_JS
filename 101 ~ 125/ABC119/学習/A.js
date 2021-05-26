6a148cf369edf4fdfe2bfca09e3372c9d1493b5f
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-26 15:13:17 +0900

    test

--- 101 ~ 125/ABC119/学習/A.js
+++ 101 ~ 125/ABC119/学習/A.js
@@ -3,17 +3,16 @@
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
-    'use strict';
+    "use strict";
     let s = new Date(input);
-    let t = new Date('2019/04/30');
+    let t = new Date("2019/04/30");
 
-    if(s <= t) {
+    if (s <= t) {
         console.log("Heisei");
     } else {
         console.log("TBD");
     }
 }
 
-
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+main(require("fs").readFileSync("../txt/A.txt", "utf8"));
