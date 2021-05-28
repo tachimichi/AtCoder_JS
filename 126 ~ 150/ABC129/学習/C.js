e534431c40ea8172de09cd62955cdd3d91ca0d5f
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-28 14:25:21 +0900

    DP（動的計画法）

--- 126 ~ 150/ABC129/学習/C.js
+++ 126 ~ 150/ABC129/学習/C.js
@@ -1,5 +1,5 @@
 // TODO
-//*   ABC 130 C - Rectangle Cutting
+//*   ABC 129 C - Typical Stairs
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
@@ -8,8 +8,8 @@ function main(input) {
     const [n, m] = input[0].trim().split(' ').map(n => parseInt(n, 10));
     //* -----------------------------
     let a = [];
-    let oks = new Array(n+1).fill(true);
-    for(let i = 1; i <= m; i++) {
+    let oks = new Array(n + 1).fill(true);
+    for (let i = 1; i <= m; i++) {
         let num = parseInt(input[i], 10);
         a.push(num);
         //* 壊れている階段をfalseにする
@@ -19,15 +19,15 @@ function main(input) {
     console.log(oks);
 
     //* ------------
-    let mod = 1e9+7;
-    let dp = new Array(n+1).fill(0);
+    let mod = 1e9 + 7;
+    let dp = new Array(n + 1).fill(0);
     dp[0] = 1;
 
-    for(let now = 0; now < n; now++) {
+    for (let now = 0; now < n; now++) {
         //* j<最終ゴールのnか、二歩先のnow+2のどちらか迄
-        for(let next = now+1; next <= Math.min(n, now+2); next++) {
+        for (let next = now + 1; next <= Math.min(n, now + 2); next++) {
             //* oks[next]がtrue（＝渡れる場合）
-            if(oks[next]) {
+            if (oks[next]) {
                 dp[next] += dp[now];
                 dp[next] %= mod;
             }
