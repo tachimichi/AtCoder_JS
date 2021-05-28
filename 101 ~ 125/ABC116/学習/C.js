63a5f3302f88583a211844ea49a1c0e1df3f932e
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-28 14:20:02 +0900

    読み込み不足

--- 101 ~ 125/ABC116/学習/C.js
+++ 101 ~ 125/ABC116/学習/C.js
@@ -1,27 +1,18 @@
 // TODO
-//*   ABC 117 C - Streamline
+//*   ABC 116 C - Grand Garden
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const [n, m] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const n = parseInt(input[0], 10);
     const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
 
-    a.sort((a, b) => a - b);
-
-    let t = [];
-    for (let i = 0; i < m - 1; i++) {
-        let x = a[i + 1] - a[i];
-        t.push(x);
+    let sum = a[0];
+    for (let i = 0; i < n - 1; i++) {
+        if (a[i] < a[i + 1]) sum += a[i + 1] - a[i];
     }
-    t.sort((a, b) => b - a);
-
-    let ans = 0;
-    for (let i = n - 1; i < t.length; i++) {
-        ans += t[i];
-    }
-    console.log(ans);
+    console.log(sum);
 }
 
 
