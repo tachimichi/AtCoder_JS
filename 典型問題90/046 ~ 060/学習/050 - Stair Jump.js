9a2ec239ef657c99b0aa70b95e6883b49386864f
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-28 15:02:12 +0900

    動的計画法

--- 典型問題90/046 ~ 060/学習/050 - Stair Jump.js
+++ 典型問題90/046 ~ 060/学習/050 - Stair Jump.js
@@ -1,11 +1,25 @@
 // TODO
-//*    048 - I will not drop out
+//*    050 - Stair Jump
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [n, l] = input.split(' ').map(n => parseInt(n, 10));
+    const [n, L] = input.split(' ').map(n => parseInt(n, 10));
+    const mod = 1e9 + 7;
 
+    //* 1で初期化する
+    let dp = new Array(n + 1).fill(1);
+    for (let i = 1; i <= n; i++) {
+        if (i < L) {
+            //* 1歩ずつ歩くため、1通り
+            dp[i] = dp[i - 1];
+        } else {
+            //* n-1段目から1段登るか、n-L段目からL段登るか
+            dp[i] = (dp[i - 1] + dp[i - L]) % mod;
+        }
+    }
+    //* 最後のn番目の値が答え
+    console.log(dp[n]);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
