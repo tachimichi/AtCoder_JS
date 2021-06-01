da3e3a294d9aa92741b1400481347eccf1cd4532
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-01 17:22:34 +0900

    map処理を2つ実装

---
+++ 101 ~ 125/ABC110/提出/B_提出.js
@@ -0,0 +1,36 @@
+// TODO
+//*   ABC 110 B - 1 Dimensional World's Tale
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const [n, m, x, y] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    const b = input[2].trim().split(' ').map(n => parseInt(n, 10));
+
+    a.sort((a, b) => a - b);
+    b.sort((a, b) => a - b);
+
+    //* お互いが干渉していないかチェックする
+    if (a[a.length - 1] < y && x < b[0] && a[a.length - 1] < b[0]) {
+        console.log("No War");
+    } else {
+        console.log("War");
+    }
+
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
---
+++ 101 ~ 125/ABC110/提出/C_提出.js
@@ -0,0 +1,53 @@
+// TODO
+//*   ABC 110 C - String Transformation
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const s = input[0].trim().split('');
+    const t = input[1].trim().split('');
+
+    // console.log(s);
+    // console.log(t);
+    let map1 = new Map();
+    let map2 = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        //* map1にs[i]のキーを持っているかどうか
+        if (map1.has(s[i])) {
+            //* s[i]が保持しているmap1にt[i]のアルファベットを正しく持っているかどうか
+            if (map1.get(s[i]) != t[i]) {
+                return console.log("No");
+            }
+        } else {
+            //* 無い場合は、セットする
+            map1.set(s[i], t[i]);
+        }
+
+        //* map2にt[i]のキーを持っているかどうか
+        if (map2.has(t[i])) {
+            //* s[i]が保持しているmap2にt[i]のアルファベットを正しく持っているかどうか
+            if (map2.get(t[i]) != s[i]) {
+                return console.log("No");
+            }
+        } else {
+            //* 無い場合は、セットする
+            map2.set(t[i], s[i]);
+        }
+    }
+    console.log("Yes");
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
