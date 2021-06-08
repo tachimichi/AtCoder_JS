029afea2b4f7b45577140b3b1a9424c5fa23a95c
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-08 11:50:46 +0900

    n % nの各桁の和 == 0かどうか

--- 101 ~ 125/ABC101/提出/B_提出.js
+++ 101 ~ 125/ABC101/提出/B_提出.js
@@ -1,16 +1,20 @@
 // TODO
-//*   ABC 102 B - Maximum Difference
+//*   ABC 101 B - Digit Sums
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
-    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
-    a.sort((a, b) => a - b);
-    console.log(a[a.length - 1] - a[0]);
+    const n = parseInt(input, 10);
+    const keta_sum = input.trim().split('').reduce((a, b) => parseInt(a, 10) + parseInt(b, 10));
+    // console.log(keta_sum);
 
+    if (n % keta_sum == 0) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
 }
+
 
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
