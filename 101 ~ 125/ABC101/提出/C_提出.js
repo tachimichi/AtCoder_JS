b0966918e042ce70ab15e3441348afe67e8bc20a
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-09 17:02:31 +0900

    区間の選び方

--- 101 ~ 125/ABC101/提出/C_提出.js
+++ 101 ~ 125/ABC101/提出/C_提出.js
@@ -1,32 +1,35 @@
 // TODO
-//*   ABC 102 C - Linear Approximation
+//*   ABC 101 C - Minimization
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
+    const [n, k] = input[0].trim().split(' ').map(n => parseInt(n, 10));
     const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
-    // console.log(a);
-
-    let b = [];
-    for (let i = 0; i < n; i++) {
-        b[i] = a[i] - i - 1;
-    }
-    // console.log(b);
-
-    //* 中央値を求める
-    b.sort((a, b) => a - b);
-    let x = b[Math.floor(n / 2)];
-    // console.log(x);
-    // console.log(b);
-
-    let ans = 0;
-    for (let i = 0; i < n; i++) {
-        ans += Math.max(x - b[i], b[i] - x);
-    }
+    //* 解法1：
+    const ans = Math.floor((n - 1 + k - 2) / (k - 1));
     console.log(ans);
+
+    //* 解法2
+    //* 交差している区間は互いに行き来できるものとする。
+    //* 最小個数の区間を置いて、1〜N全体を行き来できるようにせよ。
+
+    let ans2 = 0;
+    let right = 0;
+    while (true) {
+        if (ans2 == 0) {
+            right += k;
+        } else {
+            right += k - 1;
+        }
+        ans2++;
+
+        if (right >= n) break;
+    }
+    // console.log(ans2);
 }
+
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
