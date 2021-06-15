66081b74784c13e755091e9af88b72864e0eed83
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-16 08:41:37 +0900

    読み込み修正

--- 076 ~ 100/ABC098/学習/A.js
+++ 076 ~ 100/ABC098/学習/A.js
@@ -16,4 +16,4 @@ function main(input) {
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
