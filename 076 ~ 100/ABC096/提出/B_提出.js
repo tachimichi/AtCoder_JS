1900d64f555d2ae60f156806234bcb2bdc2ce71a
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-18 08:57:05 +0900

    b意外と実装に時間かかった

--- 076 ~ 100/ABC096/提出/B_提出.js
+++ 076 ~ 100/ABC096/提出/B_提出.js
@@ -1,25 +1,25 @@
 // TODO
-//*   ABC 097 B - Exponential
+//*   ABC 096 B - Maximum Sum
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
+    let t = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const k = parseInt(input[1], 10);
 
-    if (n == 1) return console.log(1);
-
-    let ans = 0;
-    for (let i = 2; i <= 100; i++) {
-        for (let j = 2; i ** j <= n; j++) {
-            if (ans <= i ** j) {
-                ans = i ** j;
+    for (let i = 0; i < k; i++) {
+        let max = Math.max(...t);
+        for (let j = 0; j < t.length; j++) {
+            if (t[j] == max) {
+                t[j] = max * 2;
+                //* 一回のみ
+                break;
             }
         }
     }
-    console.log(ans);
+    console.log(t.reduce((a, b) => a + b));
 }
-
 
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
