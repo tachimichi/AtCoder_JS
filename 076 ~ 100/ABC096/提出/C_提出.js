155d1ce7cc0371d3109e1ef821bdfb4b544ca683
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-06-18 09:26:53 +0900

    h, w図形の典型

--- 076 ~ 100/ABC096/提出/C_提出.js
+++ 076 ~ 100/ABC096/提出/C_提出.js
@@ -1,30 +1,38 @@
 // TODO
-//*   ABC 097 C - K-th Substring
+//*   ABC 096 C - Grid Repainting 2
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const s = input[0].trim();
-    const n = parseInt(input[1], 10);
+    const [h, w] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    //* 最初と最後に"."を入れて処理できるようにする
+    let t = [];
+    t.push(new Array(w + 2).fill("."));
+    for (let i = 1; i <= h; i++) {
+        const a = input[i].trim().split('');
+        a.unshift(".")
+        a.push(".")
+        t.push(a);
+    }
+    t.push(new Array(w + 2).fill("."));
+    // console.log(t);
 
-    let list = [];
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < s.length; j++) {
-            // console.log(i, i + j + 1);
-            list.push(s.substring(i, i + j + 1));
+    let count = 0;
+    for (let i = 0; i < t.length; i++) {
+        for (let j = 0; j < t[i].length; j++) {
+            //* 上下左右に"#"がない場合、カウントする
+            if (t[i][j] == "#" &&
+                t[i][j - 1] != "#" &&
+                t[i - 1][j] != "#" &&
+                t[i][j + 1] != "#" &&
+                t[i + 1][j] != "#") count++;
         }
     }
-
-    // console.log(list);
-    list.sort();
-    // console.log(list);
-    let ans = new Set(list);
-    list = [...ans];
-    // console.log(list);
-    console.log(list[n - 1]);
+    //* 三項演算子
+    //* 条件 ? 真に対する実行 : 偽に対する実行
+    console.log(count ? "No" : "Yes");
 }
-
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
