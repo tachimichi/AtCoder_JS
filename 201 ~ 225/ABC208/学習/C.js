10cba86dc719eada65c9245d0f9ab84633e0085b
tachi_michi@revolve <defxyz714@gmail.com>
2021-07-12 09:33:13 +0900

    c

--- 201 ~ 225/ABC208/学習/C.js
+++ 201 ~ 225/ABC208/学習/C.js
@@ -1,42 +1,31 @@
 // TODO
-//*   ABC 207 C - Many Segments
+//*   ABC 209 C - Not Equal
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
     const n = parseInt(input[0], 10);
-    let s = [];
-    for (let i = 1; i <= n; i++) {
-        let [t, l, r] = input[i].trim().split(' ').map(n => parseInt(n, 10));
-        //* -----------------------
-        if (t == 2) {
-            r -= 0.5;
-        } else if (t == 3) {
-            l += 0.5;
-        } else if (t == 4) {
-            l += 0.5;
-            r -= 0.5;
-        }
-        //* -----------------------
-        s.push([t, l, r]);
-    }
-    // console.log(s);
+    const c = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    const mod = 1000000007n;
+    c.sort((a, b) => a - b);
+    console.log(n);
+    console.log(c);
 
-    let ans = 0;
+    //* 解法：
+    //* C = (100, 100, 100)のとき
+    //* A1 = 100, A2 = 99, A3 = 98通り
+    //* ⇒重複する値はNGのため
+
+    let ans = 1n;
     for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            // console.log(i, j);
-            // console.log(s[i][1], s[j][1]);
-            // console.log(s[i][2], s[j][2]);
-            if (Math.max(s[i][1], s[j][1]) <= Math.min(s[i][2], s[j][2])) {
-                // console.log("OK");
-                ans++;
-            }
-            // console.log('-------------');
-        }
+        //* c[i]が重複する個数iより、小さい場合
+        if (c[i] - i < 1) ans = 0n;
+        //* 
+        ans = (ans * BigInt(c[i] - i)) % mod;
     }
-    console.log(ans);
+    console.log(ans.toString());
+
 }
 
 
--- 201 ~ 225/ABC209/学習/C.js
+++ 201 ~ 225/ABC209/学習/C.js
@@ -1,12 +1,30 @@
 // TODO
-//*   ABC 207 C - Many Segments
+//*   ABC 209 C - Not Equal
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
     const n = parseInt(input[0], 10);
-    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    const c = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    const mod = 1000000007n;
+    c.sort((a, b) => a - b);
+    // console.log(n);
+    // console.log(c);
+
+    //* 解法：
+    //* C = (100, 100, 100)のとき
+    //* A1 = 100, A2 = 99, A3 = 98通り
+    //* ⇒重複する値はNGのため
+
+    let ans = 1n;
+    for (let i = 0; i < n; i++) {
+        //* c[i]が重複する個数iより、小さい場合
+        if (c[i] - i < 1) ans = 0n;
+        //* 
+        ans = (ans * BigInt(c[i] - i)) % mod;
+    }
+    console.log(ans.toString());
 
 }
 
