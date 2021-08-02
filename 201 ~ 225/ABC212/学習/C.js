3a48347a72d9ae2a55e7f6b81461d4d72e1f28de
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-08-02 21:53:15 +0900

    212

--- 201 ~ 225/ABC212/学習/C.js
+++ 201 ~ 225/ABC212/学習/C.js
@@ -1,30 +1,12 @@
 // TODO
-//*   ABC 209 C - Not Equal
+//*   ABC 212 C - Min Difference
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
-    const c = input[1].trim().split(' ').map(n => parseInt(n, 10));
-    const mod = 1000000007n;
-    c.sort((a, b) => a - b);
-    // console.log(n);
-    // console.log(c);
+    const [n, m] = input[0].trim().split(' ').map(n => parseInt(n, 10));
 
-    //* 解法：
-    //* C = (100, 100, 100)のとき
-    //* A1 = 100, A2 = 99, A3 = 98通り
-    //* ⇒重複する値はNGのため
-
-    let ans = 1n;
-    for (let i = 0; i < n; i++) {
-        //* c[i]が重複する個数iより、小さい場合
-        if (c[i] - i < 1) ans = 0n;
-        //* 
-        ans = (ans * BigInt(c[i] - i)) % mod;
-    }
-    console.log(ans.toString());
 
 }
 
