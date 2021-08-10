3debd99ff19172e280e599ba3b1ff8e5794e2f04
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-10 17:59:42 +0900

    084

---
+++ 076 ~ 100/ABC084/学習/A.js
@@ -0,0 +1,15 @@
+// TODO
+//*   ABC 084 A - New Year
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    // input = input.trim().split('\n');
+    const n = parseInt(input.trim(), 10);
+    console.log((24 - n) + 24);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+
+
