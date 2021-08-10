9009eaf03bde0808d0366af8eb5a21a4d6246ac7
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-08-10 22:28:32 +0900

    084B

--- 076 ~ 100/ABC084/提出/B_提出.js
+++ 076 ~ 100/ABC084/提出/B_提出.js
@@ -1,23 +1,21 @@
 // TODO
-//*   ABC 085 B - Kagami Mochi
+//*   ABC 084 B - Postal Code
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
-    const d = [];
-    for (let i = 1; i <= n; i++) {
-        const element = parseInt(input[i], 10);
-        d.push(element);
+    const [a, b] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const str = input[1].trim().split('-');
+    // for (let i = 0; i < a; i++) {
+    //     if (str[i])
+    // }
+    // console.log(str);
+    if (str[0].length != a || str[1].length != b) {
+        return console.log('No');
     }
-    d.sort((a, b) => b - a);
-    // console.log(d);
-    const ans = new Set(d);
-    console.log(ans.size);
-
+    console.log('Yes');
 }
-
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
