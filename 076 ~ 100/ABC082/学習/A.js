9a95761bd16b66ac81f6291bd70421442a63d5c2
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-11 10:19:02 +0900

    082AB

---
+++ 076 ~ 100/ABC082/学習/A.js
@@ -0,0 +1,15 @@
+// TODO
+//*   ABC 082 A - Round Up the Mean
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    // input = input.trim().split('\n');
+    const [a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
+    console.log(Math.ceil((a + b) / 2));
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+
+
---
+++ 076 ~ 100/ABC082/学習/B.js
@@ -0,0 +1,22 @@
+// TODO
+//*   ABC 082 B - Two Anagrams
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    let a = input[0].trim().split('');
+    let b = input[1].trim().split('');
+    a.sort(); // 文字列ソート昇順
+    b.sort().reverse(); // 文字列ソート降順
+    // console.log(a.join(''));
+    // console.log(b);
+    if (a.join('') < b.join('')) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
