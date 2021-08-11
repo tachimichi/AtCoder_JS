0775c480dfc1fabb686d263f7f235435111de295
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-11 09:16:00 +0900

    083B

--- 076 ~ 100/ABC083/学習/B.js
+++ 076 ~ 100/ABC083/学習/B.js
@@ -6,10 +6,21 @@ function main(input) {
     'use strict';
     // input = input.trim().split('\n');
     const [n, a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
-
-
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        let keta = 0;
+        let loop = i;
+        while (loop > 0) {
+            keta += loop % 10;
+            loop = Math.floor(loop / 10);
+            // console.log(loop);
+        }
+        if (a <= keta && keta <= b) {
+            ans += i;
+        }
+    }
+    console.log(ans);
 }
-
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
--- 076 ~ 100/ABC083/提出/B_提出.js
+++ 076 ~ 100/ABC083/提出/B_提出.js
@@ -1,20 +1,25 @@
 // TODO
-//*   ABC 084 B - Postal Code
+//*   ABC 083 B - Some Sums
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('\n');
-    const [a, b] = input[0].trim().split(' ').map(n => parseInt(n, 10));
-    const str = input[1].trim().split('-');
-    // for (let i = 0; i < a; i++) {
-    //     if (str[i])
-    // }
-    // console.log(str);
-    if (str[0].length != a || str[1].length != b) {
-        return console.log('No');
+    // input = input.trim().split('\n');
+    const [n, a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        let keta = 0;
+        let loop = i;
+        while (loop > 0) {
+            keta += loop % 10;
+            loop = Math.floor(loop / 10);
+            // console.log(loop);
+        }
+        if (a <= keta && keta <= b) {
+            ans += i;
+        }
     }
-    console.log('Yes');
+    console.log(ans);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
