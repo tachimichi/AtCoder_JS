043feab2a45b8323ae474c17202715e8071b17d0
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-12 14:47:10 +0900

    080A

---
+++ 076 ~ 100/ABC080/学習/A.js
@@ -0,0 +1,16 @@
+// TODO
+//*   ABC 080 A - Parking
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    // input = input.trim().split('\n');
+    const [n, a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
+    console.log(Math.min((n * a), b));
+
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+
+
---
+++ 076 ~ 100/ABC080/提出/B_提出.js
@@ -0,0 +1,42 @@
+// TODO
+//*   ABC 081 B - Shift only
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const n = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    // console.log(a);
+    let isEven = true;
+    let count = 0;
+    while (isEven) {
+        for (let i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                a[i] /= 2;
+            } else {
+                // console.log(a[i]);
+                isEven = false;
+            }
+        }
+        if (isEven) {
+            count++;
+        } else {
+            break;
+        }
+    }
+    console.log(count);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
