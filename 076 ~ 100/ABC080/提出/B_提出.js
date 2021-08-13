77798d0bd8c169b4f2e33c48060812f4ee27e53c
tachi_michi@revolve <defxyz714@gmail.com>
2021-08-13 13:20:36 +0900

    080B

--- 076 ~ 100/ABC080/提出/B_提出.js
+++ 076 ~ 100/ABC080/提出/B_提出.js
@@ -1,31 +1,19 @@
 // TODO
-//*   ABC 081 B - Shift only
+//*   ABC 080 B - Harshad Number
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('\n');
-    const n = input[0].trim().split(' ').map(n => parseInt(n, 10));
-    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
-    // console.log(a);
-    let isEven = true;
-    let count = 0;
-    while (isEven) {
-        for (let i = 0; i < a.length; i++) {
-            if (a[i] % 2 == 0) {
-                a[i] /= 2;
-            } else {
-                // console.log(a[i]);
-                isEven = false;
-            }
-        }
-        if (isEven) {
-            count++;
-        } else {
-            break;
-        }
+    // input = input.trim().split('\n');
+    const n = input.trim().split(' ').map(n => parseInt(n, 10));
+    const a = input.trim().split('').map(n => parseInt(n, 10));
+    // console.log(n);
+    let x = 0;
+    for (let i = 0; i < a.length; i++) {
+        x += a[i];
     }
-    console.log(count);
+    // console.log(x);
+    console.log(n % x == 0 ? "Yes" : "No");
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
