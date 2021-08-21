4c58a79db93848d16ae0f0bcae72a3a78f3bdf5d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-08-21 23:24:27 +0900

    215ABC

---
+++ 201 ~ 225/ABC215/提出/B_提出.js
@@ -0,0 +1,31 @@
+// TODO
+//*   ABC 215
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    // input = input.trim().split('\n');
+    const n = BigInt(input.trim());
+    let ans = 0;
+    for (let i = 0n; i < n; i++) {
+        const x = 2n ** i;
+        if (x <= n) {
+            ans = i;
+        } else {
+            break;
+        }
+    }
+    console.log(ans.toString());
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
