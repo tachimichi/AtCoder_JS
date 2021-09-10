f6647d64dd39ae85a473f120314d7d83201c840d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-09-10 14:15:06 +0900

    216AB

--- 201 ~ 225/ABC216/学習/B.js
+++ 201 ~ 225/ABC216/学習/B.js
@@ -1,21 +1,29 @@
 // TODO
-//*   ABC 215
+//*   ABC 216 B - Same Name
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    // input = input.trim().split('\n');
-    const n = BigInt(input.trim());
-    let ans = 0;
-    for (let i = 0n; i < n; i++) {
-        const x = 2n ** i;
-        if (x <= n) {
-            ans = i;
-        } else {
-            break;
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    let t = [];
+    for (let i = 1; i <= n; i++) {
+        const [x, y] = input[i].trim().split(' ');
+        t.push([x, y]);
+    }
+    // console.log(t);
+    let isOK = false;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (t[i][0] == t[j][0]) {
+                if (t[i][1] == t[j][1]) {
+                    isOK = true;
+                }
+            }
         }
     }
-    console.log(ans.toString());
+    console.log(isOK ? 'Yes' : 'No');
 }
 
 
