bfaa77d6ff3e51b8ae36c2800230f981ed50ef49
tachi_michi@revolve <defxyz714@gmail.com>
2021-12-23 11:11:09 +0900

    227

--- 226 ~ 250/ABC227/学習/A.js
+++ 226 ~ 250/ABC227/学習/A.js
@@ -1,19 +1,14 @@
 // TODO
-//*   ABC 225 A - Distinct Strings
+//*   ABC 227 A - Last Card
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let str = input.trim().split('');
-    let setStr = new Set(str);
-
-    if (setStr.size == 1) {
-        console.log(1);
-    } else if (setStr.size == 2) {
-        console.log(3);
-    } else {
-        console.log(6);
-    }
+    const [n, k, a] = input.trim().split(' ').map(n => parseInt(n, 10));
+    let ans = (a + k - 1) % n;
+    if (ans == 0) ans = n;
+    console.log(ans);
 }
+
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
