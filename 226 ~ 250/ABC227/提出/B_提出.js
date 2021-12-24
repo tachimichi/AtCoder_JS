7725bcc5616269c53d77afa975bcc9a195b258fa
tachi_michi@revolve <defxyz714@gmail.com>
2021-12-24 16:40:41 +0900

    set.has(element)の使用方法

--- 226 ~ 250/ABC227/提出/B_提出.js
+++ 226 ~ 250/ABC227/提出/B_提出.js
@@ -1,26 +1,28 @@
 // TODO
-//*   ABC 225 B - Star or Not
+//*   ABC 227 B - KEYENCE building
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
     const n = parseInt(input[0], 10);
-    let array = new Array(n).fill(0);
-
-    for (let i = 1; i < n; i++) {
-        const [a, b] = input[i].trim().split(' ').map(n => parseInt(n, 10));
-        array[a - 1] += 1;
-        array[b - 1] += 1;
-    }
-
-    let ans = 'No';
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] == n - 1) {
-            ans = 'Yes';
+    const s = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    // console.log(s);
+    let areaSet = new Set();
+    for (let i = 0; i < n; i++) {
+        for (let a = 1; a <= s[i] - 1; a++) {
+            for (let b = 1; b < s[i] - a; b++) {
+                let x = 4 * a * b + 3 * (a + b);
+                if (x == s[i]) {
+                    areaSet.add(x);
+                }
+            }
         }
     }
-    console.log(ans);
+    // console.log(areaSet);
+    // 除外処理（特定の値がセット内に存在するか）
+    const ans = s.filter(element => !areaSet.has(element));
+    console.log(ans.length);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
