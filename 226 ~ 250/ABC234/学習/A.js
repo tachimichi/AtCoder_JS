3a7bc55568f6aee0780a9058adc23429ec5686d1
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-13 12:01:51 +0900

    関数の呼び出し

--- 226 ~ 250/ABC234/学習/A.js
+++ 226 ~ 250/ABC234/学習/A.js
@@ -1,11 +1,17 @@
 // TODO
-//*   ABC 232 A - QQ solver
+//*   ABC 231 A - Weird Function
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [a, b] = input.trim().split('x').map(n => parseInt(n, 10));
-    console.log(a * b);
+    const t = parseInt(input.trim(), 10);
+    let ans = f(f(f(t) + t) + f(f(t)));
+    console.log(ans);
+
+    function f(num) {
+        return num * num + 2 * num + 3;
+    }
+
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
