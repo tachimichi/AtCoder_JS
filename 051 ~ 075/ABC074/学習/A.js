14808e85ac9b16004ec60da0f012a7229eb45c95
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-17 17:31:27 +0900

    B意味、理解できていない

---
+++ 051 ~ 075/ABC074/学習/A.js
@@ -0,0 +1,15 @@
+// TODO
+//*   ABC 074 A - Bichrome Cells
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const a = parseInt(input[1], 10);
+    console.log(n * n - a);
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
---
+++ 051 ~ 075/ABC074/提出/A_提出.js
@@ -0,0 +1,24 @@
+// TODO
+//*   ABC 074 A - Bichrome Cells
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const a = parseInt(input[1], 10);
+    console.log(n * n - a);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
