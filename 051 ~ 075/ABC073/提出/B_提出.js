d0b5a26ae11a3adb389988a69d4bda6d0fc26d3d
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-18 08:54:55 +0900

    席の総和

--- 051 ~ 075/ABC073/提出/B_提出.js
+++ 051 ~ 075/ABC073/提出/B_提出.js
@@ -1,66 +1,18 @@
 // TODO
-//*   ABC 075 B - Minesweeper
+//*   ABC 073 B - Theater
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const [h, w] = input[0].trim().split(' ').map(n => parseInt(n, 10));
-
-    let t = [];
-    let ans = [];
-    for (let i = 1; i <= h; i++) {
-        //標準入力用
-        const a = input[i].trim().split('');
-        a.unshift('.');
-        a.push('.');
-        t.push(a);
-        //カウント用
-        const list = new Array(w).fill(0);
-        list.unshift(0);
-        list.push(0);
-        ans.push(list);
+    const n = parseInt(input[0], 10);
+    let ans = 0;
+    for (let i = 1; i <= n; i++) {
+        const [l, r] = input[i].trim().split(' ').map(n => parseInt(n, 10));
+        const t = r - l + 1;
+        ans += t;
     }
-    //余白計算用
-    const mod = new Array(w + 2).fill('.');
-    t.unshift(mod);
-    t.push(mod);
-
-    const zero = new Array(w + 2).fill(0);
-    ans.unshift(zero);
-    ans.push(zero);
-    // ---------------------------------------------
-
-    for (let i = 1; i < h + 1; i++) {
-        for (let j = 1; j < w + 1; j++) {
-            if (t[i][j] == '#') {
-                // console.log(i, j);
-
-                ans[i - 1][j - 1]++;
-                ans[i - 1][j]++;
-                ans[i - 1][j + 1]++;
-                ans[i][j - 1]++;
-                ans[i][j + 1]++;
-                ans[i + 1][j - 1]++;
-                ans[i + 1][j]++;
-                ans[i + 1][j + 1]++;
-            }
-        }
-    }
-    for (let i = 1; i < h + 1; i++) {
-        for (let j = 1; j < w + 1; j++) {
-            if (t[i][j] == '#') {
-                ans[i][j] = '#';
-            }
-        }
-    }
-    ans.shift();
-    ans.pop();
-    for (let i = 0; i < ans.length; i++) {
-        ans[i].shift();
-        ans[i].pop();
-        console.log(ans[i].join(''));
-    }
+    console.log(ans);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
