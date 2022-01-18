921cdc12ebbf48fbae033ae77ef14bd05ec99d80
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-19 08:31:37 +0900

    4重ループ

--- 201 ~ 225/ABC224/提出/B_提出.js
+++ 201 ~ 225/ABC224/提出/B_提出.js
@@ -1,24 +1,36 @@
 // TODO
-//*   ABC 222 B - Failing Grade
+//*   ABC 224 B - Mongeness
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let str = input.trim().split('');
-    let tmp = [];
-    // console.log(str);
-    for (let i = 0; i < str.length; i++) {
-        // 要素の末尾を取得
-        let b = str.pop();
-        // 取得した末尾を先頭に追加
-        str.unshift(b);
-        // シフトした配列を、tmpに追加
-        tmp.push(str.join(''));
+    input = input.trim().split('\n');
+    const [h, w] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    let t = [];
+    for (let i = 1; i <= h; i++) {
+        const a = input[i].trim().split(' ').map(n => parseInt(n, 10));
+        t.push(a);
     }
-    tmp.sort();
-    console.log(tmp[0]);
-    console.log(tmp[tmp.length - 1]);
+
+    for (let i1 = 0; i1 < h; i1++) {
+        for (let i2 = 0; i2 < h; i2++) {
+            for (let j1 = 0; j1 < w; j1++) {
+                for (let j2 = 0; j2 < w; j2++) {
+                    // console.log(i1, i2, j1, j2);
+                    if (i1 < i2 && j1 < j2) {
+                        // console.log(i1, i2, j1, j2);
+                        if (t[i1][j1] + t[i2][j2] > t[i2][j1] + t[i1][j2]) {
+                            return console.log('No');
+                        }
+                    }
+                }
+                // console.log("=======");
+            }
+        }
+    }
+    console.log("Yes");
 }
+
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
