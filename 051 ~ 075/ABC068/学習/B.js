39a93e3d9f4ebae8d0b7c5572626d581e5fd0dee
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-27 09:44:17 +0900

    2乗の最大値

--- 051 ~ 075/ABC068/学習/B.js
+++ 051 ~ 075/ABC068/学習/B.js
@@ -1,13 +1,16 @@
 // TODO
-//*   ABC 069 B - i18n
+//*   ABC 068 B - Break Number
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let str = input.trim().split('');
-    const first = str.shift();
-    const last = str.pop();
-    console.log(first + str.length + last);
+    const n = parseInt(input, 10);
+    if (n == 1) return console.log(1);
+    let num = 1;
+    while (n >= num * 2) {
+        num *= 2;
+    }
+    console.log(num);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
