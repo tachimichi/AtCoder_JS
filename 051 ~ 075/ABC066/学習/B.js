3f1a3472c968de6fc07cd902dae8abd246276cfd
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-28 20:56:44 +0900

    067B

--- 051 ~ 075/ABC066/学習/B.js
+++ 051 ~ 075/ABC066/学習/B.js
@@ -1,13 +1,20 @@
 // TODO
-//*   ABC 069 B - i18n
+//*   ABC 067 B - Snake Toy
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let str = input.trim().split('');
-    const first = str.shift();
-    const last = str.pop();
-    console.log(first + str.length + last);
+    input = input.trim().split(' ').map(n => parseInt(n, 10));
+    const [n, k] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const l = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    l.sort((a, b) => b - a);
+
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += l[i];
+    }
+    console.log(sum);
+
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
