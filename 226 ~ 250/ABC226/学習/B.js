3d712efd8f8f7472cf7dd82dc98bc41e795523e8
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-31 17:01:55 +0900

    多重配列の重複チェック

--- 226 ~ 250/ABC226/学習/B.js
+++ 226 ~ 250/ABC226/学習/B.js
@@ -1,26 +1,21 @@
 // TODO
-//*   ABC 225 B - Star or Not
+//*   ABC 226 B - Counting Arrays
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
     const n = parseInt(input[0], 10);
-    let array = new Array(n).fill(0);
-
-    for (let i = 1; i < n; i++) {
-        const [a, b] = input[i].trim().split(' ').map(n => parseInt(n, 10));
-        array[a - 1] += 1;
-        array[b - 1] += 1;
+    const lst = [];
+    for (let i = 1; i <= n; i++) {
+        const a = input[i].trim().split(' ').map(n => parseInt(n, 10));
+        lst.push(a);
     }
-
-    let ans = 'No';
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] == n - 1) {
-            ans = 'Yes';
-        }
-    }
-    console.log(ans);
+    // console.log(lst);
+    // 多重配列の重複チェック
+    const ans = [...new Set(lst.map(JSON.stringify))].map(JSON.parse);
+    // console.log(ans);
+    console.log(ans.length);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
