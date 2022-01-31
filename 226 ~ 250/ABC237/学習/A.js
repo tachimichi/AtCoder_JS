bc286b7b96831608cfdcc959ac0742f259013ef4
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-31 09:14:35 +0900

    237b

---
+++ 226 ~ 250/ABC237/学習/A.js
@@ -0,0 +1,17 @@
+// TODO
+//*   ABC 236 A - chukodai
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const str = input[0].trim().split('');
+    const [a, b] = input[1].trim().split(' ').map(n => parseInt(n, 10));
+    let temp = str[a - 1];
+    str[a - 1] = str[b - 1];
+    str[b - 1] = temp;
+    console.log(str.join(''));
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
---
+++ 226 ~ 250/ABC237/提出/B_提出.js
@@ -0,0 +1,35 @@
+// TODO
+//*   ABC 237 B - Matrix Transposition
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const [h, w] = input[0].trim().split(' ').map(n => parseInt(n, 10));
+    const k = [];
+    for (let i = 1; i <= h; i++) {
+        k.push(input[i].trim().split(' ').map(n => parseInt(n, 10)));
+    }
+    // console.log(k);
+    const a = [];
+    for (let i = 0; i < w; i++) {
+        const b = [];
+        for (let j = 0; j < h; j++) {
+            b.push(k[j][i]);
+        }
+        console.log(b.join(' '));
+    }
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
