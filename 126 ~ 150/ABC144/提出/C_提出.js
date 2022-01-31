d71c57c92962d91f7061f1f59f4a0815c8f2cb0b
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-31 17:52:16 +0900

    フォーマット

--- 126 ~ 150/ABC144/提出/C_提出.js
+++ 126 ~ 150/ABC144/提出/C_提出.js
@@ -8,15 +8,15 @@ function main(input) {
     let ans = BigInt(10e12); //* maxの値
 
     //? 整数i,jの小さい方は、√n以下の数字となる
-    for (let i = 1n; i*i <= n; i++) {
-        
+    for (let i = 1n; i * i <= n; i++) {
+
         //* n % i(=a)で余りが出る場合
-        if(n % i != 0) continue;
+        if (n % i != 0) continue;
 
         //* n % a == 0のとき、b = n/aとなる
-        let j = BigInt((n/i).toString());
+        let j = BigInt((n / i).toString());
 
-        ans = Math.min(ans.toString(), (i+j-2n).toString());
+        ans = Math.min(ans.toString(), (i + j - 2n).toString());
 
     }
     console.log(ans);
@@ -27,7 +27,7 @@ try {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
 } catch (error1) {
     try {
-    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+        main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
     } catch (error2) {
         console.log('error2', error2);
         console.log('----------------------------');
