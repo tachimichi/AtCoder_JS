e906b236a5821bf8e959eaf8fc2783672454e8ab
tachi_michi@revolve <defxyz714@gmail.com>
2022-02-01 17:44:32 +0900

    065茶色

--- 051 ~ 075/ABC065/学習/B.js
+++ 051 ~ 075/ABC065/学習/B.js
@@ -1,22 +1,32 @@
 // TODO
-//*   ABC 066 B - ss
+//*   ABC 065 B - Trained?
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const a = input.trim();
-    let max = 0;
-    for (let i = 0; i < a.length; i += 2) {
-        // https://www.sejuku.net/blog/21155
-        // str.substr( 開始位置, 文字数 );
-        let first = a.substr(0, i / 2);
-        let second = a.substr(i / 2, i / 2);
-        // console.log(first, second);
-        if (first == second) {
-            max = Math.max(max, i);
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 0);
+    let a = [];
+    for (let i = 1; i <= n; i++) {
+        const x = parseInt(input[i], 10);
+        a.push(x);
+    }
+    // console.log(a);
+
+    let arr = new Array(n).fill(0);
+    let index = 0;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+
+        index = a[index] - 1;
+        arr[index]++;
+        count++;
+        if (index == 1) {
+            return console.log(count);
+        } else if (arr[index] == 2) {
+            return console.log(-1);
         }
     }
-    console.log(max);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
