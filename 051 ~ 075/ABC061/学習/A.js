14db9bcec64803e4cc878f1443bc102889defabf
tachi_michi@revolve <defxyz714@gmail.com>
2022-02-03 17:47:08 +0900

    061a

---
+++ 051 ~ 075/ABC061/学習/A.js
@@ -0,0 +1,12 @@
+// TODO
+//*   ABC 061 A - Between Two Integers
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const [a, b, c] = input.trim().split(' ').map(n => parseInt(n, 10));
+    console.log(a <= c && c <= b ? 'Yes' : 'No');
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
