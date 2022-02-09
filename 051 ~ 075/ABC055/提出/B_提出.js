861df5fb996f9b7b8b3ca1f39f05d3bc1ead017d
tachi_michi@revolve <defxyz714@gmail.com>
2022-02-09 09:15:15 +0900

    mod 考え方

--- 051 ~ 075/ABC055/提出/B_提出.js
+++ 051 ~ 075/ABC055/提出/B_提出.js
@@ -1,18 +1,18 @@
 // TODO
-//*   ABC 056 B - NarrowRectanglesEasy
+//*   ABC 055 B - Training Camp
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [w, a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
-    let min = Math.min(a, b);
-    let max = Math.max(a, b);
-    let ans = max - (w + min);
-    if (ans <= 0) {
-        console.log(0);
-    } else {
-        console.log(ans);
+    const n = BigInt(input.trim());
+    const mod = 1000000007n;
+    let ans = 1n;
+
+    for (let i = 1n; i <= n; i += 1n) {
+        ans *= i;
+        ans %= mod;
     }
+    console.log(ans.toString());
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
