1b0e72e4948f6d5428715f51f333f78e6f85f054
tachi_michi@revolve <defxyz714@gmail.com>
2022-06-01 09:03:41 +0900

    ABC249B配列

--- 226 ~ 250/ABC249/提出/B_提出.js
+++ 226 ~ 250/ABC249/提出/B_提出.js
@@ -1,17 +1,34 @@
 // TODO
-//*   ABC 248 B - Slimes
+//*   ABC 249 B - Perfect String
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let [a, b, k] = input.trim().split(' ').map(n => parseInt(n, 10));
-    let count = 0;
-    if (a >= b) return console.log(count);
-    while (true) {
-        count++;
-        a *= k;
-        if (a >= b) return console.log(count);
+    const str = input.trim().split('');
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+    let arrCount = new Array(alphabet.length).fill(0);
+    let countABC = 0;
+    let countabc = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < alphabet.length; j++) {
+            if (str[i] == alphabet[j]) {
+                arrCount[j]++;
+                if (j <= 25) {
+                    countABC++;
+                } else {
+                    countabc++;
+                }
+            }
+        }
+        // console.log(arrCount);
     }
+    for (let i = 0; i < arrCount.length; i++) {
+        if (arrCount[i] > 1 || countABC == 0 || countabc == 0) {
+            return console.log(('No'));
+        }
+    }
+    console.log('Yes');
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
