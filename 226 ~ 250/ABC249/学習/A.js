da9448732715659c380331acd648fef2e413a919
tachi_michi@revolve <defxyz714@gmail.com>
2022-06-10 08:59:23 +0900

    249A 難しい

--- 226 ~ 250/ABC249/学習/A.js
+++ 226 ~ 250/ABC249/学習/A.js
@@ -5,24 +5,37 @@
 function main(input) {
     'use strict';
     const [a, b, c, d, e, f, x] = input.trim().split(' ').map(n => parseInt(n, 10));
-    let count1 = (a + c) % x;
-    let count2 = (a + c) % x;
-    if (a < x) {
-        count1 = Math.floor((a + c) / x);
-    } else {
-        count1 = 0;
-    }
-    if (b < x) {
-        count2 = Math.floor((d + f) / x);
-    } else {
-        count2 = 0;
-    }
-    // console.log(count1, count2);
 
-    const len1 = b * count1;
-    const len2 = e * count2;
-    if (len1 == len2) return console.log("Draw");
-    console.log(len1 > len2 ? "Takahashi" : "Aoki");
+    let run1 = 0;
+    let time1 = x;
+    while (time1 > 0) {
+        if (time1 >= a) {
+            time1 -= a;
+            run1 += a;
+        } else {
+            run1 += time1;
+            break;
+        }
+        time1 -= c;
+    }
+    let run2 = 0;
+    let time2 = x;
+    while (time2 > 0) {
+        if (time2 >= d) {
+            time2 -= d;
+            run2 += d;
+        } else {
+            run2 += time2;
+            break;
+        }
+        time2 -= f;
+    }
+
+    let len1 = run1 * b;
+    let len2 = run2 * e;
+    if (len1 == len2) return console.log('Draw');
+    console.log(len1 > len2 ? 'Takahashi' : 'Aoki');
+
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
--- 226 ~ 250/ABC249/提出/A_提出.js
+++ 226 ~ 250/ABC249/提出/A_提出.js
@@ -4,10 +4,38 @@
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const n = input.trim().split('').map(n => parseInt(n, 10));
-    for (let i = 0; i < 10; i++) {
-        if (!n.includes(i)) return console.log(i);
+    const [a, b, c, d, e, f, x] = input.trim().split(' ').map(n => parseInt(n, 10));
+
+    let run1 = 0;
+    let time1 = x;
+    while (time1 > 0) {
+        if (time1 >= a) {
+            time1 -= a;
+            run1 += a;
+        } else {
+            run1 += time1;
+            break;
+        }
+        time1 -= c;
     }
+    let run2 = 0;
+    let time2 = x;
+    while (time2 > 0) {
+        if (time2 >= d) {
+            time2 -= d;
+            run2 += d;
+        } else {
+            run2 += time2;
+            break;
+        }
+        time2 -= f;
+    }
+
+    let len1 = run1 * b;
+    let len2 = run2 * e;
+    if (len1 == len2) return console.log('Draw');
+    console.log(len1 > len2 ? 'Takahashi' : 'Aoki');
+
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
