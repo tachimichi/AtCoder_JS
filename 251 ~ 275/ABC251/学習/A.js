eafc2adba1228f7be009ea1acc7d3f5d71657ea6
tachi_michi@revolve <defxyz714@gmail.com>
2022-06-10 16:47:48 +0900

    dsd

--- 251 ~ 275/ABC251/学習/A.js
+++ 251 ~ 275/ABC251/学習/A.js
@@ -10,6 +10,15 @@ function main(input) {
     if (input.length == 1) console.log(input.repeat(6));
     if (input.length == 2) console.log(input.repeat(3));
     if (input.length == 3) console.log(input.repeat(2));
+
+    const lists = [
+        { id: '000', name: 'Aさん', age: '21', content: '特になし1' },
+        { id: '001', name: 'Bさん', age: '22', content: '特になし2' },
+        { id: '002', name: 'Cさん', age: '23', content: '特になし3' }
+    ];
+    console.log(lists[0].id);
+    console.log(lists[1].id);
+    console.log(lists[2].id);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
