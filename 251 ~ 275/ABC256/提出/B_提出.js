a77536a49b061d0eaaf57d020003ba473dfe85b1
tachi_michi@revolve <defxyz714@gmail.com>
2022-06-24 17:07:16 +0900

    256B

--- 251 ~ 275/ABC256/提出/B_提出.js
+++ 251 ~ 275/ABC256/提出/B_提出.js
@@ -1,24 +1,25 @@
 // TODO
-//*   ABC 254 B - Practical Computing
+//*   ABC 256 B - Batters
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const n = parseInt(input.trim(), 10);
-    let ans = [];
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const a = input[1].trim().split(' ').map(n => parseInt(n, 10));
+
+    let count = 0;
+    let t = [];
     for (let i = 0; i < n; i++) {
-        let row = [];
-        for (let j = 0; j <= i; j++) {
-            if (j == 0 || j == i) {
-                row.push(1);
-            } else {
-                row.push(ans[i - 1][j - 1] + ans[i - 1][j]);
-            }
+        t.push(0);
+        for (let j = 0; j < t.length; j++) {
+            t[j] += a[i];
         }
-        // console.log(row);
-        ans.push(row);
-        console.log(ans[i].join(' '));
     }
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] > 3) count++;
+    }
+    console.log(count);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
