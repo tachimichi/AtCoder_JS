e39db6130204db3c91c2376dde3774935bbd6e70
tachi_michi@revolve <defxyz714@gmail.com>
2022-06-24 17:28:55 +0900

    050A

---
+++ 026 ~ 050/ABC051/学習/A.js
@@ -0,0 +1,15 @@
+// TODO
+//*   ABC 050 A - Addition and Subtraction Easy
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split(' ');
+    const a = parseInt(input[0], 10);
+    const op = input[1].trim()
+    const b = parseInt(input[2], 10);
+    console.log(op == '+' ? a + b : a - b);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
---
+++ 026 ~ 050/ABC051/学習/B.js
@@ -0,0 +1,25 @@
+// TODO
+//*   ABC 052 B - Increment Decrement
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    const s = input[1].trim().split('');
+
+    let ans = 0;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        if (s[i] == 'I') {
+            ans++;
+        } else {
+            ans--;
+        }
+        count = Math.max(count, ans);
+    }
+    console.log(count > 0 ? count : 0);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
